19c68016d84995460ce78f834e844bce2b5c7860
R Bagus Sasutya <adminpendidikanaas.operator@gmail.com>
2026-07-25 23:33:25 +0700

    Add password visibility toggle (eye icon) in auth forms

--- auth.js
+++ auth.js
@@ -482,3 +482,17 @@ window.saveUser = async function() {
   btn.innerHTML = '<i class="fas fa-save"></i> Simpan';
   btn.disabled = false;
 };
+
+window.togglePassword = function(inputId, iconId) {
+  const input = document.getElementById(inputId);
+  const icon = document.getElementById(iconId);
+  if (input.type === 'password') {
+    input.type = 'text';
+    icon.classList.remove('fa-eye');
+    icon.classList.add('fa-eye-slash');
+  } else {
+    input.type = 'password';
+    icon.classList.remove('fa-eye-slash');
+    icon.classList.add('fa-eye');
+  }
+};
